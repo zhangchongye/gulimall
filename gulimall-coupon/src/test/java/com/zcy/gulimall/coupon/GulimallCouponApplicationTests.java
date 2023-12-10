8d5ac96dda1ecd7fec2fc2d10e08189062457ab7
zhangchongye <1986884761@qq.com>
2023-12-10 21:49:18 +0800

    创建module

---
+++ gulimall-coupon/src/test/java/com/zcy/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zcy.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
